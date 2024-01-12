91e642328766a3c4e14dc26e95e61bde5d0ff407
alifbinhossain <alifahmed47@gmail.com>
2024-01-13 02:33:02 +0600

    ci: :mag: changed open-graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,24 @@ import '../styles/globals.css';
 
 import { cn } from '@/lib/utils';
 import { fontPoppins } from '@/lib/fonts';
+import { siteConfig } from '@/config/site';
 
 import Navbar from '@/components/layout/navbar/navbar';
 import { BottomNavbar } from '@/components/layout/bottom-navbar';
 import { Toaster } from '@/components/ui/sonner';
 import { ShopProvider } from '@/components/providers/shop-provider';
-import { siteConfig } from '@/config/site';
 
 export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
 };
 
 export default function RootLayout({
@@ -22,7 +30,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
+      <head />
       <body
         className={cn(
           'min-h-screen bg-background font-poppins antialiased',

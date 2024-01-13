7f86db0e6774880f1309f16befa1265157fe410b
alifbinhossain <alifahmed47@gmail.com>
2024-01-13 06:22:27 +0600

    ci: :sparkles: added lottie-file for showing empty cart state

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -2,6 +2,10 @@
 
 import Image from 'next/image';
 
+import Lottie from 'react-lottie';
+
+import LottieFile from '../../../public/assets/lotties/empty-cart.json';
+
 import {
   Sheet,
   SheetClose,
@@ -20,12 +24,15 @@ import { siteConfig } from '@/config/site';
 
 export const Cart = () => {
   const { cart, getCartProducts } = useShopContext();
+  const numberOfProducts = cart.length;
   return (
     <div className='flex items-center gap-4 bg-white'>
       <Sheet>
         <SheetTrigger asChild>
           <Button size={'icon'} variant={'ghost'} className='relative'>
-            <Badge className='absolute -top-1 -right-1 '>{cart.length}</Badge>
+            <Badge className='absolute -top-1 -right-1 '>
+              {numberOfProducts}
+            </Badge>
             <Image
               width={24}
               height={24}
@@ -37,43 +44,72 @@ export const Cart = () => {
 
         <SheetContent className=' h-full lg:max-w-[440px] flex flex-col bg-white '>
           <SheetHeader>
-            <SheetTitle>Shopping Cart ({cart.length})</SheetTitle>
+            <SheetTitle>Shopping Cart ({numberOfProducts})</SheetTitle>
           </SheetHeader>
 
           <div className='flex-1 overflow-auto scroll pr-8 divide-y'>
-            {getCartProducts(siteConfig.popularProducts).map((product) => {
-              return <CartItem key={product.name} product={product} />;
-            })}
-          </div>
-          <SheetFooter className='flex flex-col gap-2'>
-            <div className='flex items-center justify-between'>
-              <p className='text-base font-normal text-gray-900'>
-                {cart.length} Product
-              </p>
-              <p className='text-base font-semibold text-gray-900'>
-                $
-                {getCartProducts(siteConfig.popularProducts)
-                  .reduce((a, b) => a + b.price * b.cartQuantity, 0)
-                  .toFixed(2)}
-              </p>
-            </div>
+            {numberOfProducts === 0 && (
+              <div className='flex flex-col gap-8 items-center justify-center h-full text-gray-500'>
+                <div className='flex flex-col gap-8 items-center justify-center'>
+                  <Lottie
+                    height={300}
+                    width={300}
+                    options={{
+                      animationData: LottieFile,
+                      loop: true,
+                      autoplay: true,
+                      rendererSettings: {
+                        preserveAspectRatio: 'xMidYMid slice',
+                      },
+                    }}
+                  />
 
-            <div className='mt-5 space-y-3'>
-              <SheetClose asChild>
-                <Button className='w-full rounded-full' type='submit'>
-                  Checkout
+                  <p>Your cart is empty!</p>
+                </div>
+
+                <Button className='rounded-full text-base px-8 py-4 h-fit'>
+                  Go to Shop
                 </Button>
-              </SheetClose>
-              <SheetClose asChild>
-                <Button
-                  className='w-full rounded-full bg-green-gray-50 hover:bg-green-gray-100 text-primary'
-                  type='submit'
-                >
-                  Go To Cart
-                </Button>
-              </SheetClose>
-            </div>
-          </SheetFooter>
+              </div>
+            )}
+
+            {numberOfProducts > 0 &&
+              getCartProducts(siteConfig.popularProducts).map((product) => {
+                return <CartItem key={product.name} product={product} />;
+              })}
+          </div>
+
+          {numberOfProducts > 0 && (
+            <SheetFooter className='flex flex-col gap-2'>
+              <div className='flex items-center justify-between'>
+                <p className='text-base font-normal text-gray-900'>
+                  {numberOfProducts} Product
+                </p>
+                <p className='text-base font-semibold text-gray-900'>
+                  $
+                  {getCartProducts(siteConfig.popularProducts)
+                    .reduce((a, b) => a + b.price * b.cartQuantity, 0)
+                    .toFixed(2)}
+                </p>
+              </div>
+
+              <div className='mt-5 space-y-3'>
+                <SheetClose asChild>
+                  <Button className='w-full rounded-full' type='submit'>
+                    Checkout
+                  </Button>
+                </SheetClose>
+                <SheetClose asChild>
+                  <Button
+                    className='w-full rounded-full bg-green-gray-50 hover:bg-green-gray-100 text-primary'
+                    type='submit'
+                  >
+                    Go To Cart
+                  </Button>
+                </SheetClose>
+              </div>
+            </SheetFooter>
+          )}
         </SheetContent>
       </Sheet>
       <div>
